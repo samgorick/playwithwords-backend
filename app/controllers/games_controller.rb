01f8ce560b0001d236bcb5d86520a6ba46133142
gozmeister <samgorick.sg@gmail.com>
2020-05-26 11:56:39 -0500

    linked front and backend

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,13 @@
 class GamesController < ApplicationController
+
+  def create
+    game = Game.create!(game_params)
+    render json: game
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:word_list, :score, :user_id, :letter_list_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.find_or_create_by!(name: params[:name])
+    render json: user
+    # render json: UserSerializer.new(user)
+  end
+  
 end
