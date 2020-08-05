c3a41728004dabedeed61af6ca7ef16638a08a14
gozmeister <samgorick.sg@gmail.com>
2020-08-05 12:49:56 -0500

    minor tidy up to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   def create
     user = User.find_or_create_by!(name: params[:name])
     render json: user, include: [:games, :letter_lists] 
-    # render json: UserSerializer.new(user)
   end
 
 end
